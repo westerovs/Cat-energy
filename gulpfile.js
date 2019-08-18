9b88be7aa85997887223c87a5f341dcd7f2948b3
westerovs <westerovs@mail.ru>
2019-08-18 14:24:18 +0300

    всё ок

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
-var csso = require('gulp-csso');
-var rename = require("gulp-rename");
+var csso = require("gulp-csso");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
@@ -16,6 +16,9 @@ var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
 
+gulp.task("clean", function () {
+  return del("build");
+})
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -24,50 +27,12 @@ gulp.task("css", function () {
     .pipe(less())
     .pipe(postcss([
       autoprefixer()
-  ]))
-  .pipe(csso())
-  .pipe(rename("style.css"))
-  .pipe(sourcemap.write("."))
-  .pipe(gulp.dest("build/css"));
-});
-
-// оптимизация картинок
-gulp.task("images", function () {
-  return gulp.src("source/img/**/*.{png,jpg,svg}")
-   .pipe(imagemin([
-     imagemin.optipng({optimizationLevel: 1}),
-     imagemin.jpegtran({progressive: true}),
-     imagemin.svgo()
-   ]))
-    .pipe(gulp.dest("source/img"));
-});
-
-// конвертирует в webp
-gulp.task("webp", function () {
-  return gulp.src("source/img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"));
-});
-
-// делает спрайты
-gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
-    .pipe(svgstore({
-    inlineSvg: true
-    }))
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
-});
-
-/* тэг инклюд - он вставляет спрайт
-<div style="display:none">
-  <include src="source/img/sprite.svg"></include>
-</div> */
-gulp.task("html", function () {
-  return gulp.src("source/*.html")
-  .pipe(posthtml([
-    include()]))
-  .pipe(gulp.dest("build"));
+    ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
 });
 
 gulp.task("copy", function () {
@@ -75,57 +40,71 @@ gulp.task("copy", function () {
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
     "source/js/**",
-    "source/css/normalize.css",
     "source/*.ico"
   ], {
     base: "source"
-   })
+  })
   .pipe(gulp.dest("build"));
+})
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
 });
 
-//Очищаем папку build перед копированием
-gulp.task("clean", function () {
- return del("build");
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
 });
 
-// ***
-// обновление в браузере
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("source/img"));
+});
+
 gulp.task("server", function () {
   server.init({
-  server: "build/"
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
-  gulp.watch("source/less/blocks/*.less", gulp.series("html", "refresh"));
-  });
+});
 
 gulp.task("refresh", function (done) {
   server.reload();
   done();
 });
 
-
-// build - запускает ПОСЛЕДОВАТЕЛЬНО другие задачи
-// сперва css и тд
 gulp.task("build", gulp.series(
- "clean",
- "copy",
- "css",
- "sprite",
- "html"
+  "clean",
+  "copy",
+  "css",
+  "sprite",
+  "html"
 ));
 
-/*
-запуская npm start:
-> запустим gulp start
-> который запустит build
-> который запустит "css", "sprite", "html"
-> и в конце gulp server
-
-т.е npm start - автомат. соберётся
-css, sprite, этот спрайт будет вставлен в html
-и после этого запустится лок.сервер
-*/
 gulp.task("start", gulp.series("build", "server"));
